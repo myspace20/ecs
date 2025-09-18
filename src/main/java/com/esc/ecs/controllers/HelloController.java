f391eea0b0df9a750f5ecb46c7b82eaf9a1b4889
rogersatsi <rogersatsi98@gmail.com>
2025-09-18 19:50:07 +0000

    feat: add s3 to return value

--- src/main/java/com/esc/ecs/controllers/HelloController.java
+++ src/main/java/com/esc/ecs/controllers/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "App is running with Spring Boot after test";
+        return "App is running with Spring Boot after test after s3";
     }
     @GetMapping("/health")
     public String health() {
