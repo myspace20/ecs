39fe9f5b7d5590fc3fa060f7ed67e1451fe80397
rogersatsi <rogersatsi98@gmail.com>
2025-09-18 20:04:01 +0000

    feat: user image placeholder

--- src/main/java/com/esc/ecs/controllers/WelcomeController.java
+++ src/main/java/com/esc/ecs/controllers/WelcomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WelcomeController {
     @GetMapping
     public String welcome() {
-        return "redirect:/index.html";
+        return "index.html";
     }
 }
