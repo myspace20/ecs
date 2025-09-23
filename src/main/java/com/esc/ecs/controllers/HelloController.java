0a59e37b4452b03d8a699aea8c9ccc5f86e1c7e4
rogersatsi <rogersatsi98@gmail.com>
2025-09-23 11:01:00 +0000

    feat:use controller!

--- src/main/java/com/esc/ecs/controllers/HelloController.java
+++ src/main/java/com/esc/ecs/controllers/HelloController.java
@@ -1,17 +1,14 @@
 package com.esc.ecs.controllers;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/")
     public String hello() {
         return "redirect:/index.html";
-    }
-    @GetMapping("/health")
-    public String health() {
-        return "App is healthy!";
     }
 }
