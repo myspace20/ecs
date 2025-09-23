5626af87e8281dd3eb63ee2854b3e1ffab327847
rogersatsi <rogersatsi98@gmail.com>
2025-09-23 14:59:55 +0000

    feat: remove unused import

--- src/main/java/com/esc/ecs/controllers/HelloController.java
+++ src/main/java/com/esc/ecs/controllers/HelloController.java
@@ -3,7 +3,6 @@ package com.esc.ecs.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
